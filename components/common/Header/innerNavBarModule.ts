0ffb9f13b9eefafec91581f9002313f0de3f6608
imeureka <lky062@naver.com>
2023-07-15 00:12:08 +0900

    feat(innerMenu) : 사이드메뉴 기능 구현 및 header 스타일 수정

---
+++ components/common/Header/innerNavBarModule.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const inner_menu_bar = styled.nav`
+  display: flex;
+`;
+
+export const vertical_line = styled.nav`
+  border-left: 1px solid #E1E1E1;
+  height: 80vh; /* 원하는 높이 설정 */
+  justify-content: center;
+  align-items: center;
+`;
+
+export const inner_nav_ul = styled.nav`
+  padding  : 5%;
+`;
+
+export const inner_nav_item = styled.li`
+  font-size: 18px;
+  font-weight: 600;
+  color: #979797;
+  margin-top: 90px;
+  list-style: none;
+`;
+export const menuIcon = styled.div`
+  width: 100vw;
+`
+export const LogoBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  left: 15%;
+  position: absolute;
+`;
+
+export const ImageWrapper = styled.div`
+  position: absolute;
+`;
+
