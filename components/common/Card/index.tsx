832b3d0040ba100dadd5446f72101f5e22f144ee
Jeon Eonseok <jseokeon@gmail.com>
2023-07-15 20:46:52 +0900

    feat(Card): 재사용 가능한 Card 컴포넌트 생성

---
+++ components/common/Card/index.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+interface ICardProps {
+  width: string;
+};
+
+const CardBox = styled.div<ICardProps>`
+  width: ${props => props.width || '38.5rem'};
+  height: 65vh;
+  border-radius: 3rem;
+  background: rgba(255, 255, 255, 0.20);
+  box-shadow: 0px 1.1rem 2rem 0px rgba(0, 0, 0, 0.10);
+  backdrop-filter: blur(.2rem);
+`
+
+/** 카드 컴포넌트 */
+const Card = ({ width, children }: PropsWithChildren<ICardProps>) => {
+  return (
+    <CardBox width={width}>{children}</CardBox>
+  );
+};
+
+export default Card;
