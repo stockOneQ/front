dc5b290e100b62a0dcbd2486fc09e5205c19c13f
Jeon Eonseok <jseokeon@gmail.com>
2023-07-17 13:57:38 +0900

    fix(friends): 친구 목록 수정 체크박스 정렬 맞춤

--- components/community/Friends/Profiles/FriendsList/FriendProfile/style.ts
+++ components/community/Friends/Profiles/FriendsList/FriendProfile/style.ts
@@ -21,8 +21,8 @@ const CheckBoxButton = styled.button<ICheckBoxButtonProps>`
 const FriendProfileBox = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 0 2.5rem 0 2rem;
-  /* margin-bottom: 2.5rem; */
 `
 
 /**********************/
