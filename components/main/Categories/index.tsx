d0bb88bb2ee231da73354d878e7c3129f78685a3
imeureka <lky062@naver.com>
2023-07-26 23:29:54 +0900

    feat(categories) : 제품검색 api 및 카테고리 컴포넌트화

--- components/main/Categories/index.tsx
+++ components/main/Categories/index.tsx
@@ -1,102 +1,126 @@
-// import Card from 'components/common/Card';
-// import Image from 'next/image';
-// import Link from "next/link";
-// import toggleButtonIcon from 'public/assets/icons/community/toggleButtonIcon.svg';
-// import searchIcon from 'public/assets/icons/community/searchIcon.svg';
-// import { useState } from 'react';
-// import styled from 'styled-components';
-// import * as S from './style';
-// import { useRecoilValue, useRecoilState } from "recoil";
+// components/Categories/index.tsx
+import React, { useState } from 'react';
+import DropDown from '../../common/DropDown/index';
+import * as S from '../Ingredients/style';
+import searchIcon from 'public/assets/icons/community/searchIcon.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
-// import { gradientsListState,selectedCategoryState } from "../../../recoil/states";
+type ControlBarProps = {
+    activeLink: string;
+    //개수 조회
+    totalCount: number;
+    expiredIngredientsCount: number;
+    approachingExpirationCount: number;
+    insufficientIngredientsCount: number;
+    //정렬
+    sortOptionList: string[];
+    handleLinkClick: (link: string) => void;
+    handleSortChange: (selectedOption: string) => void;
+    handleSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    searchTerm: string;
+};
 
+const Categories: React.FC<ControlBarProps> = ({
+    activeLink,
+    totalCount,
+    expiredIngredientsCount,
+    approachingExpirationCount,
+    insufficientIngredientsCount,
+    sortOptionList,
+    handleLinkClick,
+    handleSortChange,
+    handleSearchChange,
+    searchTerm,
+}) => {
+    const [linksVisible, setLinksVisible] = useState(true);
+    const [inputWidth, setInputWidth] = useState(158); 
+    const [inputHeight, setInputHeight] = useState(30); 
 
-// const sortOptionList = ["가나다순", "빈도순"];
+    const toggleLinksVisibility = () => {
+        setLinksVisible((prevState) => !prevState);
+        if (linksVisible) {
+            setInputWidth(155); 
+        }
+    };
 
-// /** 메인 화면 카테고리 */
-// const Categories = () => {
-//     const [searchBy, setSearchBy] = useState('가나다순'); // 카테고리 선택
-//     const [categoryToggle, setCategoryToggle] = useState(false); // 카테고리 토글
-//     const [activeLink, setActiveLink] = useState("전체");
-//     const postList = useRecoilValue(gradientsListState);
-//     const [selectedCategory, setSelectedCategory] = useRecoilState(selectedCategoryState);
+    const handleInputChange = (event) => {
+        setInputHeight(event.target.scrollHeight);
+        handleSearchChange(event);
+    };
+    return (
+        <S.ControlBar>
+            <S.NavBar>
+                <div style={{ display: linksVisible ? 'flex' : 'none' }}>
+                    <S.StyledLink
+                        isactive={activeLink === "전체"}
+                        onClick={() => handleLinkClick("전체")}
+                    >
+                        전체
+                        <S.CountValue>{totalCount}</S.CountValue>
+                    </S.StyledLink>
+                    <S.StyledLink
+                        isactive={activeLink === "afterDate"}
+                        onClick={() => handleLinkClick("afterDate")}
+                    >
+                        유통기한 지난 재료
+                        <S.CountValue>{expiredIngredientsCount}</S.CountValue>
+                    </S.StyledLink>
+                    <S.StyledLink
+                        isactive={activeLink === "beforeDate"}
+                        onClick={() => handleLinkClick("beforeDate")}
+                    >
+                        유통기한 임박 재료
+                        <S.CountValue>{approachingExpirationCount}</S.CountValue>
+                    </S.StyledLink>
+                    <S.StyledLink
+                        isactive={activeLink === "no"}
+                        onClick={() => handleLinkClick("no")}
+                    >
+                        부족한 재료
+                        <S.CountValue>{insufficientIngredientsCount}</S.CountValue>
+                    </S.StyledLink>
 
-//     const expiredCount = postList.filter(
-//         (item) => item.category === "afterDate").length;
-//     const approachingExpirationCount = postList.filter(
-//         (item) => item.category === "beforeDate").length;
-//     const insufficientCount = postList.filter((item) => item.category === "no").length;
-//     const totalCount = postList.length;
+                    <S.DropBoxContainer>
+                        <DropDown
+                            width={13.3}
+                            height={3.5}
+                            fontSize={1.3}
+                            toggleSize={10}
+                            toggleTopSize={48}
+                            list={sortOptionList}
+                            onChange={handleSortChange}
+                        />
+                    </S.DropBoxContainer>
+                </div>
 
-//     const handleLinkClick = (link: string) => {
-//         setActiveLink(link);
-//     };
+                <S.SerchSection >
+                    <Image alt="search" src={searchIcon} onClick={toggleLinksVisibility} />
+                    <S.Input
+                        type="text"
+                        placeholder="제품 검색"
+                        value={searchTerm}
+                        onChange={handleInputChange}
+                        // onChange={handleSearchChange} api 호출 함수
+                        style={{
+                            display: linksVisible ? 'none' : 'block',
 
-//     const categoryToggleCloseHandler = () => {
-//         setCategoryToggle(false);
-//     }
-//     const changeValueHandler = (value: string) => {
-//         setSearchBy(value);
-//         setCategoryToggle(false);
-//     }
+                            width: `${inputWidth}px`,
+                            height: `${inputHeight}px`,
+                            transition: 'width 1s ease, height 0.3s ease', // Add transition properties
+                        }}
+                    />
+                </S.SerchSection>
 
-//     return (
-//         <>
-//             <S.ControlBar>
-//                 <S.NavBar>
-//                     <S.StyledLink
-//                         isActive={activeLink === "전체"}
-//                         onClick={() => handleLinkClick("전체")}
-//                     >
-//                         전체
-//                         <S.CountValue>{totalCount}</S.CountValue>
-//                     </S.StyledLink>
-//                     <S.StyledLink
-//                         isActive={activeLink === "afterDate"}
-//                         onClick={() => handleLinkClick("afterDate")}
-//                     >
-//                         유통기한 지난 재료
-//                         <S.CountValue>{expiredCount}</S.CountValue>
-//                     </S.StyledLink>
-//                     <S.StyledLink
-//                         isActive={activeLink === "beforeDate"}
-//                         onClick={() => handleLinkClick("beforeDate")}
-//                     >
-//                         유통기한 임박 재료
-//                         <S.CountValue>{approachingExpirationCount}</S.CountValue>
-//                     </S.StyledLink>
-//                     <S.StyledLink
-//                         isActive={activeLink === "no"}
-//                         onClick={() => handleLinkClick("no")}
-//                     >
-//                         부족한 재료
-//                         <S.CountValue>{insufficientCount}</S.CountValue>
-//                     </S.StyledLink>
-//                     {/* <S.SearchByBox> */}
-//                     <S.SelectedValueButton onClick={() => { setCategoryToggle((prev) => (!prev)) }}>
-//                         <p>{searchBy}</p>
-//                         <Image className={`${categoryToggle ? 'categoryToggle' : ''}`} src={toggleButtonIcon} alt="my_page_icon" width={12} height={10} />
-//                     </S.SelectedValueButton>
-//                     {categoryToggle && <S.OptionList>
-//                         <li onClick={() => { changeValueHandler('가나다순') }}>가나다순</li>
-//                         <li onClick={() => { changeValueHandler('빈도순') }}>빈도순</li>
-//                     </S.OptionList>}
-//                     {/* <SearchIconBox> */}
-//                     <Image alt="search" src={searchIcon}></Image>
-//                     {/* </SearchIconBox> */}
-//                     <S.ActionButtonBox>
-//                         <Link href="/new">
-//                             <S.Add>+</S.Add>
-//                         </Link>
-//                     </S.ActionButtonBox>
-//                     {/* </S.SearchByBox> */}
-//                 </S.NavBar>
+                <S.ActionButtonBox>
+                    <Link href="/new">
+                        <S.Add>+</S.Add>
+                    </Link>
+                </S.ActionButtonBox>
+            </S.NavBar>
+        </S.ControlBar>
 
-//             </S.ControlBar>
-//         </>
-//     );
-// };
+    );
+};
 
-
-
-// export default Categories;
+export default Categories;
