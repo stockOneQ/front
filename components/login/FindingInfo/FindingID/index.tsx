3f5dc0ad54b802ee254aacea5141499b8d0de252
Jeon Eonseok <jseokeon@gmail.com>
2023-07-27 11:30:46 +0900

    feat(findingID): 이메일로 찾기 UI 추가

--- components/login/FindingInfo/FindingID/index.tsx
+++ components/login/FindingInfo/FindingID/index.tsx
@@ -4,22 +4,24 @@ import DigitInput from 'components/common/loginInput/DigitInput';
 import NameInput from 'components/common/loginInput/NameInput';
 import { useState } from 'react';
 import * as S from './style';
+import EmailInput from 'components/common/loginInput/EmailInput';
 
 /** 아이디 찾기 */
 const FindingID = () => {
-  const [isSelected, setIsSelected] = useState(0);
+  const [isByDigit, setIsByDigit] = useState(true);
 
   return (
     <>
       <S.FindingIDByBox>
-        <p className={isSelected === 0 ? 'selected' : ''} onClick={() => { setIsSelected(0)} }>휴대폰으로 찾기</p>
-        <p className={isSelected === 1 ? 'selected' : ''} onClick={() => { setIsSelected(1)} }>이메일로 찾기</p>
+        <p className={isByDigit ? 'selected' : ''} onClick={() => { setIsByDigit(true)} }>휴대폰으로 찾기</p>
+        <p className={!isByDigit ? 'selected' : ''} onClick={() => { setIsByDigit(false)} }>이메일로 찾기</p>
       </S.FindingIDByBox>
       <form>
         <S.FindingIDInputBox>
           <NameInput />
           <BirthInput />
-          <DigitInput />
+          {isByDigit && <DigitInput />}
+          {!isByDigit && <EmailInput />}
         </S.FindingIDInputBox>
         {/* label, disabled, width, height, font */}
         <S.FindingIDButtonBox>
