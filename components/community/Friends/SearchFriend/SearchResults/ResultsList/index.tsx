138df8e3c40086f4f8f7e1a1e505c1d367390cc5
Jeon Eonseok <jseokeon@gmail.com>
2023-07-29 14:55:41 +0900

    feat(resultsList): 검색 결과 수 표현

--- components/community/Friends/SearchFriend/SearchResults/ResultsList/index.tsx
+++ components/community/Friends/SearchFriend/SearchResults/ResultsList/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import FriendItem from './FriendItem';
 import * as S from './style';
 import { DUMMY_DATA } from 'components/community/Friends/Profiles/FriendsList';
@@ -9,16 +10,20 @@ interface IResultsListProps {
 
 /** 검색 결과 목록 */
 const ResultsList = ({ searchBy, searchValue }: IResultsListProps) => {
+  const countRef = useRef(0);
+
   return (
     <S.ResultListBox>
       <S.ResultListTextBox>
         <p>검색 결과</p>
-        <div>4</div>
+        <div>{countRef.current}</div>
       </S.ResultListTextBox>
       <S.FriendItemsBox>
         {
           DUMMY_DATA
             .filter(({ name, location }) => {
+              countRef.current = 0;
+
               if (searchBy === '이름') {
                 return name.indexOf(searchValue) !== -1
               } else if (searchBy === '상호명') {
@@ -31,9 +36,11 @@ const ResultsList = ({ searchBy, searchValue }: IResultsListProps) => {
                 return location.slice(spaceIndex).indexOf(searchValue) !== -1;
               }
             })
-            .map(({ id, name, location, phone }) => (
-              <FriendItem key={id} name={name} location={location} phone={phone} />
-            ))
+            .map(({ id, name, location, phone }) => {
+              countRef.current += 1;
+
+              return <FriendItem key={id} name={name} location={location} phone={phone} />
+            })
         }
       </S.FriendItemsBox>
     </S.ResultListBox>
