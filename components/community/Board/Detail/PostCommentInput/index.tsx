6fab82e05bc1a6d8ea6424fb10bee8b728d6e8cf
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-02 15:38:32 +0900

    refactor(community): event type 수정, state 이름 변경

--- components/community/Board/Detail/PostCommentInput/index.tsx
+++ components/community/Board/Detail/PostCommentInput/index.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
-import { useRecoilState } from "recoil";
-import { postCommentState } from "recoil/states";
-import * as S from "./style";
-import PhotoUploadSVG from "public/assets/icons/community/photoUpload.svg";
+import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+import { postCommentInputState } from 'recoil/states';
+import * as S from './style';
+import PhotoUploadSVG from 'public/assets/icons/community/photoUpload.svg';
+import { ChangeEvent } from 'react';
 
 const PostCommentInput = () => {
-  const [comment, setComment] = useRecoilState(postCommentState);
+  const [input, setInput] = useRecoilState(postCommentInputState);
 
   return (
     <S.Box>
@@ -13,11 +14,11 @@ const PostCommentInput = () => {
         <Image src={PhotoUploadSVG} alt="upload" />
       </S.UploadButton>
       <S.Input
-        value={comment}
+        value={input}
         maxLength={1000}
-        onChange={(e: {
-          target: { value: string | ((currVal: string) => string) };
-        }) => setComment(e.target.value)}
+        onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+          setInput(e.target.value)
+        }
       />
     </S.Box>
   );
