ec734340204677f07bd67a9575573bfc9288b7b1
Jeon Eonseok <jseokeon@gmail.com>
2023-08-02 16:44:48 +0900

    docs(data): data 컴포넌트 이름 수정

---
+++ pages/connect/data/index.tsx
@@ -0,0 +1,12 @@
+// http://localhost:8080/connect/data
+
+import Data from 'components/connect/Data';
+
+/** connect - 자료 페이지 */
+const DataPage = () => {
+  return (
+    <Data />
+  );
+};
+
+export default DataPage;
