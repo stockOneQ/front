7ea23637d92484fe4650055f434c53fb087621af
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-03 18:21:09 +0900

    chore(config): [id].tsx 페이지 500 에러 해결

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-};
-
 module.exports = {
+  reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://localhost:8080/:path*",
-      },
-    ];
   },
 };
