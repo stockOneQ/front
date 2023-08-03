2c85463f8257977e8835ae7b570fb8ded30ec031
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-03 18:18:49 +0900

    refactor(community): 게시글 상세 페이지 로직 수정

--- pages/community/board/[id].tsx
+++ pages/community/board/[id].tsx
@@ -1,17 +1,32 @@
-import { useRouter } from "next/router";
-import { useRecoilValue } from "recoil";
-import { postListState } from "recoil/states";
+import { useRouter } from 'next/router';
+import { useRecoilValue } from 'recoil';
+import { postListState } from 'recoil/states';
 
-import Detail from "components/community/Board/Detail";
+import Detail from 'components/community/Board/Detail';
 
 const PostDetailPage = () => {
   const router = useRouter();
+  console.log(router.query);
   const { id } = router.query;
 
   const postList = useRecoilValue(postListState);
-  const post = postList.filter((post) => post.id === Number(id));
+  const post = postList.find(post => post.postId === Number(id));
+  console.log(post);
 
-  return <Detail postData={post[0]} />;
+  return (
+    <>
+      {post && (
+        <Detail
+          writer={post.writer}
+          uploadTime={post.uploadTime}
+          title={post.title}
+          content={post.content}
+          views={post.views}
+          likes={post.likes}
+        />
+      )}
+    </>
+  );
 };
 
 export default PostDetailPage;
