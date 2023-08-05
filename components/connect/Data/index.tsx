a7ef0ba6c8e552486b9341f45e10659cedce9769
Jeon Eonseok <jseokeon@gmail.com>
2023-08-05 16:36:13 +0900

    fix(data): 자료 페이지, 페이지네이션 위 아래 margin 값 변경

--- components/connect/Data/index.tsx
+++ components/connect/Data/index.tsx
@@ -9,8 +9,8 @@ import DataDetail from './DataDetail';
 import { useState } from 'react';
 
 const DataBox = styled.div`
-  padding: 0 1.2rem 3.6rem;
-`
+  padding: 0 1.2rem 0.1rem;
+`;
 
 const SelectSupervisorBox = styled.div`
   display: flex;
@@ -26,7 +26,7 @@ const SelectSupervisorBox = styled.div`
   font-size: 1.8rem;
   font-weight: 600;
   line-height: normal;
-`
+`;
 
 const DataNavBox = styled.div`
   display: flex;
@@ -34,7 +34,7 @@ const DataNavBox = styled.div`
   padding-bottom: 2rem;
   border-bottom: 1px solid var(--color-gray);
   position: relative;
-`
+`;
 
 const DataNav = styled.nav`
   color: #979797;
@@ -55,7 +55,7 @@ const DataNav = styled.nav`
       margin-right: 6rem;
     }
   }
-`
+`;
 
 const DataNavInputBox = styled.div`
   margin-left: auto;
@@ -69,13 +69,13 @@ const DataNavInputBox = styled.div`
     padding-left: 18.3rem;
     border-radius: 10rem;
   }
-`
+`;
 
 const DataDropDownBox = styled.div`
   position: absolute;
   top: 0;
   right: 27.3rem;
-`
+`;
 
 const Label = styled.div`
   padding: 1rem 0;
@@ -86,7 +86,7 @@ const Label = styled.div`
   .label__1 {
     width: 9.2rem;
   }
-  
+
   .label__2 {
     padding-left: 1.7rem;
   }
@@ -104,19 +104,19 @@ const Label = styled.div`
   font-size: 1.5rem;
   font-weight: 500;
   line-height: normal;
-`
+`;
 
 const LabelBox = styled(Label)`
   border-bottom: 1px solid var(--color-gray);
-  margin-bottom: .1rem;
-`
+  margin-bottom: 0.1rem;
+`;
 
 const DataListBox = styled(Label)`
   padding: 3.3rem 0;
   cursor: pointer;
 
   &:hover {
-    background-color: #F7F7F9;
+    background-color: #f7f7f9;
   }
 
   div {
@@ -135,22 +135,23 @@ const DataListBox = styled(Label)`
   .label__3 {
     color: #979797;
   }
-`
+`;
 
 const PaginationBox = styled.div`
   display: flex;
   align-items: center;
   gap: 1.5rem;
-  margin: 3rem auto 0;
+  margin: 13rem auto;
   width: 42.1rem;
-  
+
   color: var(--color-black);
   text-align: center;
   font-size: 2.2rem;
   font-weight: 500;
   line-height: normal;
-  
-  p, img {
+
+  p,
+  img {
     cursor: pointer;
   }
 
@@ -161,7 +162,7 @@ const PaginationBox = styled.div`
     justify-content: center;
     align-items: center;
   }
-  
+
   img:first-child {
     margin-right: auto;
   }
@@ -176,7 +177,7 @@ const PaginationBox = styled.div`
     background-color: var(--color-black);
     border-radius: 10rem;
   }
-`
+`;
 
 /** connect - 자료 페이지 */
 const Data = () => {
@@ -188,7 +189,12 @@ const Data = () => {
         <DataBox>
           <SelectSupervisorBox>
             <p>이혜리 슈퍼바이저</p>
-            <Image src={supervisorCategoryOpen} alt='supervisor-toggle-icon' width={8} height={7} />
+            <Image
+              src={supervisorCategoryOpen}
+              alt="supervisor-toggle-icon"
+              width={8}
+              height={7}
+            />
           </SelectSupervisorBox>
           <DataNavBox>
             <DataNav>
@@ -201,81 +207,102 @@ const Data = () => {
             </DataNav>
             <DataNavInputBox>
               <DataDropDownBox>
-                <DropDown width={16.3} height={3.5} fontSize={1.3} toggleSize={15} toggleTopSize={50} list={['글 제목', '글 내용']} />
+                <DropDown
+                  width={16.3}
+                  height={3.5}
+                  fontSize={1.3}
+                  toggleSize={15}
+                  toggleTopSize={50}
+                  list={['글 제목', '글 내용']}
+                />
               </DataDropDownBox>
-              <input type='text' />
+              <input type="text" />
             </DataNavInputBox>
           </DataNavBox>
           <LabelBox>
-            <p className='label__1'>번호</p>
-            <p className='label__2'>제목</p>
-            <p className='label__3'>작성일자</p>
-            <p className='label__4'>이해리 슈퍼바이저</p>
+            <p className="label__1">번호</p>
+            <p className="label__2">제목</p>
+            <p className="label__3">작성일자</p>
+            <p className="label__4">이해리 슈퍼바이저</p>
           </LabelBox>
           <div>
             <DataListBox>
-              <p className='label__1'>06</p>
-              <div className='label__2'>
+              <p className="label__1">06</p>
+              <div className="label__2">
                 <p>알바생 교육관련 6월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.06.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.06.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
-            <DataListBox onClick={() => { setTemp(false)} }>
-              <p className='label__1'>05</p>
-              <div className='label__2'>
+            <DataListBox
+              onClick={() => {
+                setTemp(false);
+              }}
+            >
+              <p className="label__1">05</p>
+              <div className="label__2">
                 <p>알바생 교육관련 5월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.05.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.05.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
             <DataListBox>
-              <p className='label__1'>04</p>
-              <div className='label__2'>
+              <p className="label__1">04</p>
+              <div className="label__2">
                 <p>알바생 교육관련 4월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.04.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.04.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
             <DataListBox>
-              <p className='label__1'>03</p>
-              <div className='label__2'>
+              <p className="label__1">03</p>
+              <div className="label__2">
                 <p>알바생 교육관련 3월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.03.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.03.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
             <DataListBox>
-              <p className='label__1'>02</p>
-              <div className='label__2'>
+              <p className="label__1">02</p>
+              <div className="label__2">
                 <p>알바생 교육관련 2월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.02.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.02.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
             <DataListBox>
-              <p className='label__1'>01</p>
-              <div className='label__2'>
+              <p className="label__1">01</p>
+              <div className="label__2">
                 <p>알바생 교육관련 1월 공지사항</p>
-                <Image src={fileExist} alt='file-icon' width={15} height={12} />
+                <Image src={fileExist} alt="file-icon" width={15} height={12} />
               </div>
-              <p className='label__3'>2023.01.13</p>
-              <p className='label__4'>이혜리 슈퍼바이저</p>
+              <p className="label__3">2023.01.13</p>
+              <p className="label__4">이혜리 슈퍼바이저</p>
             </DataListBox>
           </div>
           <PaginationBox>
-            <Image src={movePageL} alt='pagination-move-left' width={16} height={9} />
-            <p className='pagination__active'>1</p>
+            <Image
+              src={movePageL}
+              alt="pagination-move-left"
+              width={16}
+              height={9}
+            />
+            <p className="pagination__active">1</p>
             <p>2</p>
             <p>3</p>
             <p>4</p>
             <p>5</p>
-            <Image src={movePageR} alt='pagination-move-right' width={16} height={9} />
+            <Image
+              src={movePageR}
+              alt="pagination-move-right"
+              width={16}
+              height={9}
+            />
           </PaginationBox>
         </DataBox>
       )}
