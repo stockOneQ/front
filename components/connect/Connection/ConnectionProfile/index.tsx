12a55a97e11d00e3c3d25261e77516987e0645d4
Jeon Eonseok <jseokeon@gmail.com>
2023-08-06 18:08:11 +0900

    feat(connection): 검색 기능 구현

--- components/connect/Connection/ConnectionProfile/index.tsx
+++ components/connect/Connection/ConnectionProfile/index.tsx
@@ -2,14 +2,24 @@ import profileImg from 'public/assets/imgs/community/myProfile.png';
 import Image from 'next/image';
 import * as S from './style';
 
+interface IConnectionProfileProps {
+  name: string;
+  location: string;
+  digit: string;
+}
+
 /** 슈퍼바이저 프로필 */
-const ConnectionProfile = () => {
+const ConnectionProfile = ({
+  name,
+  location,
+  digit,
+}: IConnectionProfileProps) => {
   return (
     <S.ConnectionProfileBox>
       <Image src={profileImg} alt="user-profile-img" width={60} height={60} />
-      <p>이혜리</p>
-      <p>이디야 슈퍼바이저</p>
-      <p>010-1112-3334</p>
+      <p>{name}</p>
+      <p>{location} 슈퍼바이저</p>
+      <p>{digit}</p>
     </S.ConnectionProfileBox>
   );
 };
--- components/connect/Connection/index.tsx
+++ components/connect/Connection/index.tsx
@@ -4,32 +4,103 @@ import searchIcon from 'public/assets/icons/common/searchIcBlack.svg';
 import ConnectionProfile from './ConnectionProfile';
 import useScroll from 'hooks/useScroll';
 import * as S from './style';
+import { useState } from 'react';
+
+const DUMMY_DATA = [
+  {
+    id: 0,
+    name: '이혜리',
+    location: '이디야',
+    digit: '010-1122-3334',
+  },
+  {
+    id: 1,
+    name: '김해리',
+    location: '스타벅스',
+    digit: '010-1122-1111',
+  },
+  {
+    id: 2,
+    name: '언노운',
+    location: '메가커피',
+    digit: '010-3322-2477',
+  },
+  {
+    id: 3,
+    name: '새우',
+    location: '이디야',
+    digit: '010-2435-5665',
+  },
+  {
+    id: 4,
+    name: '길지운',
+    location: '컴포즈커피',
+    digit: '010-3475-3454',
+  },
+  {
+    id: 5,
+    name: '김지원',
+    location: '컴포즈커피',
+    digit: '010-2475-3524',
+  },
+  {
+    id: 6,
+    name: '구나연',
+    location: '맥도날드',
+    digit: '010-6655-9098',
+  },
+  {
+    id: 7,
+    name: '윤선경',
+    location: '이디야',
+    digit: '010-1245-2738',
+  },
+  {
+    id: 8,
+    name: '양채린',
+    location: '파이브가이즈',
+    digit: '010-1094-2398',
+  },
+  {
+    id: 9,
+    name: '빵빵이',
+    location: '맥도날드',
+    digit: '010-1409-4235',
+  },
+];
 
 /** connect - 연결 페이지 */
 const Connection = () => {
   const { hideScroll, scrollHandler } = useScroll();
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const onWriteHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setEnteredValue(e.currentTarget.value);
+  };
 
   return (
     <Card width="75rem" height="73.8rem" padding="2rem 2rem 5.2rem">
       <S.ConnectionHeaderBox>
         <p>나와 연결되어 있는 슈퍼바이저</p>
         <S.ConnectionInputBox>
-          <input type="text" />
+          <input type="text" onChange={onWriteHandler} />
           <Image src={searchIcon} alt="search-icon" width={20} height={20} />
         </S.ConnectionInputBox>
       </S.ConnectionHeaderBox>
       <S.ConnectionBodyBox hideScroll={hideScroll} onScroll={scrollHandler}>
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
-        <ConnectionProfile />
+        {DUMMY_DATA.filter(
+          ({ name, location, digit }) =>
+            name.includes(enteredValue) ||
+            location.includes(enteredValue) ||
+            digit.includes(enteredValue),
+        ).map(({ id, name, location, digit }) => (
+          <ConnectionProfile
+            key={id}
+            name={name}
+            location={location}
+            digit={digit}
+          />
+        ))}
       </S.ConnectionBodyBox>
     </Card>
   );
