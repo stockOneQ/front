ef08e7187baa79e828518a69733901ff66c7f896
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-07 17:42:10 +0900

    fix(community): 게시글 등록 페이지 충돌 해결

--- pages/community/board/new/index.tsx
+++ pages/community/board/new/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Editor from 'components/community/Board/New/Editor';
 
 const New = () => {
@@ -6,12 +5,3 @@ const New = () => {
 };
 
 export default New;
-=======
-import New from 'components/community/Board/New';
-
-const NewPage = () => {
-  return <New />;
-};
-
-export default NewPage;
->>>>>>> e9526d33bbf0d21f0ef1a0f37258e6acdfd952b6
