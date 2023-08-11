31d0623d8e4995d940bb38a568aa548b8eb25521
임하림 <gkfla668@naver.com>
2023-08-11 15:32:46 +0900

    design(community): 전체글 width 수정

--- components/common/HeadingText/style.ts
+++ components/common/HeadingText/style.ts
@@ -1,7 +1,7 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 export const Box = styled.div`
-  width: 100%;
+  width: 94.2%;
   padding: 10px 0px 40px 0px;
   display: flex;
   justify-content: center;
