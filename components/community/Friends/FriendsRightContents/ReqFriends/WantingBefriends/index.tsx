1b0180a5312658478dc3b13db71648326bd54c3b
Jeon Eonseok <jseokeon@gmail.com>
2023-08-13 00:13:57 +0900

    refactor(wantingBeFriends): useScroll 사용

--- components/community/Friends/FriendsRightContents/ReqFriends/WantingBefriends/index.tsx
+++ components/community/Friends/FriendsRightContents/ReqFriends/WantingBefriends/index.tsx
@@ -1,32 +1,23 @@
 import EachWantingFriend from './EachWantingFriend';
 import * as S from '../style';
-import { useEffect, useState } from 'react';
 import { DUMMY_DATA } from 'components/community/Friends/Profiles/FriendsList';
+import useScroll from 'hooks/useScroll';
 
 /** 친구 신청 목록 */
 const WantingBefriends = () => {
-  const [isScroll, setIsScroll] = useState(-100);
-  const [hideScroll, setHideScroll] = useState(true);
-
-  const scrollHandler = (e: React.UIEvent<HTMLDivElement>) => {
-    setHideScroll(false);
-    setIsScroll(e.currentTarget.scrollTop);
-  }
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setHideScroll(true);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, [isScroll])
+  const { hideScroll, scrollHandler } = useScroll();
 
   return (
     <div>
       <S.HeadParagraph>친구 신청 목록 6</S.HeadParagraph>
       <S.EachWantingFriendsBox hideScroll={hideScroll} onScroll={scrollHandler}>
         {DUMMY_DATA.map(({ id, name, location, phone }) => (
-          <EachWantingFriend key={id} name={name} location={location} phone={phone} />
+          <EachWantingFriend
+            key={id}
+            name={name}
+            location={location}
+            phone={phone}
+          />
         ))}
       </S.EachWantingFriendsBox>
     </div>
