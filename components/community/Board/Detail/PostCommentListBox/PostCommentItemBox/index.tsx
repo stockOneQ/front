5964737ca8c80a8ab9e3b9687acf111da3ff7a2b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-14 22:14:08 +0900

    feat(api): 댓글 삭제, 수정/취소 모달

--- components/community/Board/Detail/PostCommentListBox/PostCommentItemBox/index.tsx
+++ components/community/Board/Detail/PostCommentListBox/PostCommentItemBox/index.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react';
 import Image from 'next/image';
-
+import { useSetRecoilState } from 'recoil';
+import { commentsRenderTriggerState } from 'recoil/states';
 import * as S from './style';
 import profileImage from 'public/assets/imgs/community/profileImage.png';
+import CommentMoreSVG from 'public/assets/icons/community/more.svg';
+import { API } from 'pages/api/api';
 
 type ICommentType = {
+  id: number;
   writerName: string;
   content: string;
-  date: string;
+  createdDate: string | undefined;
 };
 
-const PostCommentItemBox = ({ writerName, content, date }: ICommentType) => {
+const PostCommentItemBox = ({
+  id,
+  writerName,
+  content,
+  createdDate,
+}: ICommentType) => {
+  const [isClick, setIsClick] = useState(false);
+  const setCommentRenderTrigger = useSetRecoilState(commentsRenderTriggerState);
+
+  const handleClick = () => {
+    setIsClick(prev => !prev);
+  };
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {
+    API.delete(`/api/comments/${id}`)
+      .then(() => {
+        console.log(`${id}번의 댓글 삭제 성공`);
+        setCommentRenderTrigger(prev => !prev);
+      })
+      .catch(e => {
+        console.error(e);
+        throw e;
+      });
+  };
+
   return (
     <S.Box>
       <Image src={profileImage} alt="profile" />
@@ -18,8 +49,22 @@ const PostCommentItemBox = ({ writerName, content, date }: ICommentType) => {
           <h1>{writerName} 사장님</h1>
           <span>{content}</span>
         </S.ContentContainer>
-        <S.Date>{date}</S.Date>
+        <S.Date>{createdDate}</S.Date>
       </S.Container>
+      {/** 추후 loginId랑 비교하여 더보기 버튼 띄우기 */}
+      <S.OptionButton onClick={handleClick}>
+        <Image src={CommentMoreSVG} alt="commentMore" />
+      </S.OptionButton>
+      {isClick && (
+        <S.Modal>
+          <S.Button type="수정" onClick={handleEdit}>
+            수정하기
+          </S.Button>
+          <S.Button type="삭제" onClick={handleDelete}>
+            삭제하기
+          </S.Button>
+        </S.Modal>
+      )}
     </S.Box>
   );
 };
--- components/community/Board/Detail/PostCommentListBox/PostCommentItemBox/style.ts
+++ components/community/Board/Detail/PostCommentListBox/PostCommentItemBox/style.ts
@@ -1,6 +1,8 @@
 import { styled } from 'styled-components';
 
 export const Box = styled.div`
+  position: relative;
+  width: 100%;
   display: flex;
   gap: 2.6rem;
   align-items: start;
@@ -8,6 +10,7 @@ export const Box = styled.div`
 `;
 
 export const Container = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 1.8rem;
@@ -37,4 +40,39 @@ export const Date = styled.span`
   font-size: 1.8rem;
   line-height: 2.1rem;
   color: #e1e1e1;
+`;
+
+export const OptionButton = styled.button`
+  position: absolute;
+  right: 0;
+`;
+
+export const Modal = styled.div`
+  position: absolute;
+  right: 1.8%;
+  width: 16.2rem;
+  height: 9.2rem;
+
+  background: #ffffff;
+  border: 1px solid #f7f7f9;
+  border-radius: 8px;
+`;
+export const Button = styled.button<{ type: string }>`
+  width: 100%;
+  height: 50%;
+
+  font-weight: 500;
+  font-size: 1.8rem;
+  line-height: 2.1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: ${props => (props.type === '삭제' ? '#F2B2CF' : 'black')};
+
+  &:hover {
+    background: #f7f7f9;
+    border-radius: 0.8rem;
+  }
 `;
