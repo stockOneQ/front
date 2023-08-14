1ead414c5934170bd89ddef072fe8dbd36794c9a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-08-14 22:12:51 +0900

    feat(api): 댓글 등록

--- components/community/Board/Detail/PostCommentInputBox/index.tsx
+++ components/community/Board/Detail/PostCommentInputBox/index.tsx
@@ -1,38 +1,65 @@
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import { postCommentListState, postCommentInputState } from 'recoil/states';
+import {
+  commentsRenderTriggerState,
+  postCommentInputState,
+} from 'recoil/states';
 import { getCurrentDate } from 'utils/date';
 import * as S from './style';
 import PostInfoBox from 'components/community/Board/Detail/PostInfoBox';
 import PostCommentInput from 'components/community/Board/Detail/PostCommentInput';
-
-let id = 3;
-const getId = () => {
-  return id++;
-};
+import { API } from 'pages/api/api';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const PostCommentInputBox = () => {
-  const [commentInput, setCommentInput] = useRecoilState(postCommentInputState);
-  const setPostCommentList = useSetRecoilState(postCommentListState);
+  const router = useRouter();
+  const { id } = router.query;
+  const [content, setContent] = useRecoilState(postCommentInputState);
+  const setCommentsRenderTrigger = useSetRecoilState(
+    commentsRenderTriggerState,
+  );
+  const [formData, setFormData] = useState({
+    image: '',
+    content: content,
+  });
+
+  useEffect(() => {
+    setFormData({ image: '', content: content });
+  }, [content]);
 
   const handleSubmit = () => {
-    if (commentInput.length < 1) return alert('1자 이상 입력해 주세요.');
+    const formDatas = new FormData();
+    const jsonFormData = {
+      content: formData.content,
+    };
 
-    setPostCommentList(com => [
-      ...com,
-      {
-        id: getId(),
-        writer: '임하림',
-        uploadTime: getCurrentDate(),
-        content: commentInput,
+    formDatas.append('image', '');
+    formDatas.append(
+      'request',
+      new Blob([JSON.stringify(jsonFormData)], { type: 'application/json' }),
+    );
+
+    /** ----------------- 댓글 등록 API ----------------- */
+    API.post(`/api/comments/${id}`, formDatas, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
       },
-    ]);
-
-    setCommentInput('');
+    })
+      .then(() => {
+        console.log(`${id}번 게시글에 댓글 등록 성공`);
+        setContent('');
+        setCommentsRenderTrigger(prev => !prev);
+      })
+      .catch(e => {
+        console.error(e);
+        throw e;
+      });
   };
 
   return (
     <S.InputBox>
-      <PostInfoBox writerName={'임하림'} createdDate={getCurrentDate()} />
+      {/** 추후 writerName에는 login 한 사용자 이름  */}
+      <PostInfoBox writerName={'박사장'} createdDate={getCurrentDate()} />
       <PostCommentInput />
       <S.SubmitButton onClick={handleSubmit}>
         <span>댓글 등록</span>
