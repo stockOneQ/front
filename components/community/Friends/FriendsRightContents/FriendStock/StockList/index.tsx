2d816fb2289d455563d59763215b683867c4937e
Jeon Eonseok <jseokeon@gmail.com>
2023-08-19 21:06:02 +0900

    feat(stockList): 이미지 불러오는 로직 구현

--- components/community/Friends/FriendsRightContents/FriendStock/StockList/index.tsx
+++ components/community/Friends/FriendsRightContents/FriendStock/StockList/index.tsx
@@ -4,15 +4,23 @@ import * as S from './style';
 interface IStockListProps {
   name: string;
   stockQuant: number;
-  image: null;
+  image: string;
 }
 
 /** 재고 목록 */
 const StockList = ({ name, stockQuant, image }: IStockListProps) => {
+  console.log('typeof', typeof image);
+
+  const decodedImg = atob(image);
+  const imgBlob = new Blob([
+    new Uint8Array([...decodedImg].map(char => char.charCodeAt(0))),
+  ]);
+  const imageUrl = URL.createObjectURL(imgBlob);
+
   return (
     <S.StockDataItem>
       <S.StockImgBox>
-        {/* <Image src={image} alt="stock" width={162} height={176} /> */}
+        {image && <Image src={imageUrl} alt="stock" width={162} height={176} />}
         <p>{stockQuant}</p>
       </S.StockImgBox>
       <S.StockDataParagraph>{name}</S.StockDataParagraph>
