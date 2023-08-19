23d541b0da5798355014fc49032ca3631a7fc2ae
Jeon Eonseok <jseokeon@gmail.com>
2023-08-19 23:53:47 +0900

    refactor(stockList): 이미지 받아는 코드 수정

--- components/community/Friends/FriendsRightContents/FriendStock/StockList/index.tsx
+++ components/community/Friends/FriendsRightContents/FriendStock/StockList/index.tsx
@@ -9,20 +9,17 @@ interface IStockListProps {
 
 /** 재고 목록 */
 const StockList = ({ name, stockQuant, image }: IStockListProps) => {
-  console.log('typeof', typeof image);
-
-  const decodedImg = atob(image);
-  const imgBlob = new Blob([
-    new Uint8Array([...decodedImg].map(char => char.charCodeAt(0))),
-  ]);
-  const imageUrl = URL.createObjectURL(imgBlob);
-
   return (
     <S.StockDataItem>
       <S.StockImgBox>
         {image && (
           <div>
-            <Image src={imageUrl} alt="stock" width={144} height={144} />
+            <Image
+              src={`data:image/jpeg;base64,${image}`}
+              alt="stock"
+              width={144}
+              height={144}
+            />
           </div>
         )}
         <p>{stockQuant}</p>
